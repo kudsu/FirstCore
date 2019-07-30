dd1aa7242f42030d7964e867f132a7ecee7b1053
金奎 张 <kudsu@qq.com>
2019-07-30 18:35:43 +0800

    添加项目文件。

---
+++ FirstCore/Controllers/RMFController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstCore.Controllers
+{
+    public class RMFController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ FirstCore/Controllers/TestController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstCore.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            //string name = Request.Query["name"];
+            //Request.Cookies[""];
+            //Request.Form[""];
+            //HttpContext.Session.SetInt32("age", 18);
+            //var age = HttpContext.Session.GetInt32("age");
+            //return Content("say hello!");
+
+            Customer customer = new Customer() { Id = 1, Name = "kudsu" };
+            return View(customer);
+
+        }
+        public IActionResult Login([FromForm]string username, [FromForm]string userpwd)
+        {
+            return Content($"名字是:{username};密码是:{userpwd }");
+            //return Json(new { LofinName = name });
+        }
+        public IActionResult FanXing()
+        {
+            Dictionary<string, string> keyValues = new Dictionary<string, string>();
+            keyValues.Add("1", "1");
+
+            return View();
+
+        }
+    }
+    public class Customer
+    {
+        [DisplayName("编号")]
+        public int Id { get; set; }
+        [DisplayName("名称")]
+        public string Name { get; set; }
+    }
+
+    public class News
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+    }
+}
